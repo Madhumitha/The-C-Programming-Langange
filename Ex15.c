340d29ad92528b1ea2de2ab1eeae570e2ac8326c
Madhumitha <madhumithabiw@gmail.com>
2019-06-05 12:48:59 -0500

    Add files via upload

---
+++ Ex15.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+float celsius(float fahr);
+
+int main()
+{
+	int lower, upper, step;
+
+	float fahr;
+	lower = 0;
+	upper = 300;
+	step = 20;
+
+	fahr = lower;
+	while(fahr <= upper)
+	{
+		printf("%3.0f %6.1f\n", fahr, celsius(fahr));
+		fahr = fahr + step;
+	}
+}
+
+float celsius(float fahr)
+{
+return (5.0/9.0) * (fahr-32.0);
+}
+
+
+
+
